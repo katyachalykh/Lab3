87e2abd477b7f863b857d72f2cc0e5c5e4306a08
Ekaterina <70802069+katyachalykh@users.noreply.github.com>
2020-11-05 21:51:10 +0500

    1

--- settings/iarproject.Debug.cspy.bat
+++ settings/iarproject.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "C:\Users\mircc\Desktop\Универ зло\4 курс\Колодий\Laba3\settings\iarproject.Debug.general.xcl" --backend -f "C:\Users\mircc\Desktop\Универ зло\4 курс\Колодий\Laba3\settings\iarproject.Debug.driver.xcl" 
+"D:\Проги для учебы\common\bin\cspybat" -f "C:\Users\kadyr\Desktop\Robert\Cpecialno dlyf IAR\Laba3_ChalykhKadyrov\settings\iarproject.Debug.general.xcl" --backend -f "C:\Users\kadyr\Desktop\Robert\Cpecialno dlyf IAR\Laba3_ChalykhKadyrov\settings\iarproject.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "C:\Users\mircc\Desktop\Универ зло\4 курс\Колодий\Laba3\settings\iarproject.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\mircc\Desktop\Универ зло\4 курс\Колодий\Laba3\settings\iarproject.Debug.driver.xcl" 
+"D:\Проги для учебы\common\bin\cspybat" -f "C:\Users\kadyr\Desktop\Robert\Cpecialno dlyf IAR\Laba3_ChalykhKadyrov\settings\iarproject.Debug.general.xcl" "--debug_file=%~1" --backend -f "C:\Users\kadyr\Desktop\Robert\Cpecialno dlyf IAR\Laba3_ChalykhKadyrov\settings\iarproject.Debug.driver.xcl" 
 
 @echo off 
 :end
--- settings/iarproject.Debug.cspy.ps1
+++ settings/iarproject.Debug.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "C:\Users\mircc\Desktop\Универ зло\4 курс\Колодий\Laba3\settings\iarproject.Debug.general.xcl" --backend -f "C:\Users\mircc\Desktop\Универ зло\4 курс\Колодий\Laba3\settings\iarproject.Debug.driver.xcl" 
+& "D:\Проги для учебы\common\bin\cspybat" -f "C:\Users\kadyr\Desktop\Robert\Cpecialno dlyf IAR\Laba3_ChalykhKadyrov\settings\iarproject.Debug.general.xcl" --backend -f "C:\Users\kadyr\Desktop\Robert\Cpecialno dlyf IAR\Laba3_ChalykhKadyrov\settings\iarproject.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files (x86)\IAR Systems\Embedded Workbench 8.3\common\bin\cspybat" -f "C:\Users\mircc\Desktop\Универ зло\4 курс\Колодий\Laba3\settings\iarproject.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\mircc\Desktop\Универ зло\4 курс\Колодий\Laba3\settings\iarproject.Debug.driver.xcl" 
+& "D:\Проги для учебы\common\bin\cspybat" -f "C:\Users\kadyr\Desktop\Robert\Cpecialno dlyf IAR\Laba3_ChalykhKadyrov\settings\iarproject.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\kadyr\Desktop\Robert\Cpecialno dlyf IAR\Laba3_ChalykhKadyrov\settings\iarproject.Debug.driver.xcl" 
 }
